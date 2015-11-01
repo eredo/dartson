b6884b5558d38c71d995d83c03eb0c6795499817
Jack Murphy <jackmurphy@Jacks-MacBook-Pro-2.local>
2015-11-01 16:13:56 -0500

    add double to transformer list

--- lib/src/core_transformers.dart
+++ lib/src/core_transformers.dart
@@ -9,6 +9,7 @@ final Symbol _QN_BOOL = reflectClass(bool).qualifiedName;
 final Symbol _QN_LIST = reflectClass(List).qualifiedName;
 final Symbol _QN_MAP = reflectClass(Map).qualifiedName;
 final Symbol _QN_OBJECT = reflectClass(Object).qualifiedName;
+final Symbol _QN_DOUBLE = reflectClass(double).qualifiedName;
 
 var _qn = (Type t) => _getName(reflectClass(t).qualifiedName);
 
@@ -23,6 +24,7 @@ final Map<String, TypeTransformer> _simpleTransformers = {
   _qn(List): _defaultSimpleTransformer,
   _qn(Map): _defaultSimpleTransformer,
   _qn(Object): _defaultSimpleTransformer,
+  _qn(double): _defaultSimpleTransformer,
   'dart.core._OneByteString': _defaultSimpleTransformer,
   'dart.core._Smi': _defaultSimpleTransformer
 };
