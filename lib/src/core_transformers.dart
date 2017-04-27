7a56625e2b30be5133103943f9a1960177bce864
Jiri Mikulasek <jiri.mikulasek@gooddata.com>
2017-04-27 09:46:12 +0200

    add support for _TwoByteString
    resolves #33

--- lib/src/core_transformers.dart
+++ lib/src/core_transformers.dart
@@ -20,5 +20,6 @@ final Map<String, TypeTransformer> _simpleTransformers = {
   _qn(Map): _defaultSimpleTransformer,
   _qn(Object): _defaultSimpleTransformer,
   'dart.core._OneByteString': _defaultSimpleTransformer,
+  'dart.core._TwoByteString': _defaultSimpleTransformer,
   'dart.core._Smi': _defaultSimpleTransformer
 };
