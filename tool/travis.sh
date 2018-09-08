194c2b2f4d3f1a0bb0f89ddd8de2cd2b050715fd
Eric Schneller <eric@schnellers.name>
2018-09-08 10:00:01 -0700

    Extend travis tool and remove dart tasks

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo -e '\033[1mTASK: Fetching dependencies [pub get]\033[22m'
+echo -e 'pub get'
+pub get
+
+echo -e '\033[1mTASK: Dart Analyzer [analyzer]\033[22m'
+echo -e 'dartanalyzer --fatal-warnings .'
+dartanalyzer --fatal-warnings .
+
+echo -e '\033[1mTASK: Dart Format [dartfmt]\033[22m'
+echo -e 'dartfmt -n lib/ test/ example/'
+dartfmt -n lib/ test/ example/
+
+echo -e '\033[1mTASK: Testing [test]\033[22m'
 pub run dart_coveralls report \
   --retry 2 \
   --exclude-test-files \
